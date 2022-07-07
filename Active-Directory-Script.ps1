0ff0aea2bb52ad04cd8c80d7d0eb5010938456c4
Nikhil Thakur <96281583+NikhilThakur-IT@users.noreply.github.com>
2022-07-07 18:25:54 +1000

    Add files via upload
    - Added function to split the first and last names from the list into two sub-strings

--- Active-Directory-Script.ps1
+++ Active-Directory-Script.ps1
@@ -6,4 +6,15 @@ $UserNamesList = Get-Content .\names.txt
 
 $password = ConvertTo-SecureString $PasswordForUsers -AsPlainText -Force
 
+
+# Creates an OrganisationalUnit named Users
 New-ADOrganisationalUnit -Name _USERS
+
+
+# Splitting of the names into sub-strings
+foreach ($n in $UserNamesList) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+           
+
+    }
