d7099be73736c39dfacde9d0ba1c4d9708fc5a5c
Nikhil Thakur <96281583+NikhilThakur-IT@users.noreply.github.com>
2022-07-07 18:13:02 +1000

    Add files via upload
    - Implemented SecureString password for users
    - Creates OrganisationalUnit in the Active Directory

--- Active-Directory-Script.ps1
+++ Active-Directory-Script.ps1
@@ -1,4 +1,9 @@
-﻿# 
+﻿#$PasswordForUsers is the inital password without secure string # 
 
 $PasswordForUsers = “Password123”
 $UserNamesList = Get-Content .\names.txt
+
+
+$password = ConvertTo-SecureString $PasswordForUsers -AsPlainText -Force
+
+New-ADOrganisationalUnit -Name _USERS
