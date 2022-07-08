65525b5cb912a684506eea99c4fcb3a26089ac30
Nikhil Thakur <96281583+NikhilThakur-IT@users.noreply.github.com>
2022-07-08 16:44:26 +1000

    Add files via upload
    - Added username variable which uses the first letter of the first name and adds it to the last name (in lower case). (Nikhil Thakur = nthakur)
    - Added New-AdUser cmdlet and added parameters

--- Active-Directory-Script.ps1
+++ Active-Directory-Script.ps1
@@ -8,13 +8,24 @@ $password = ConvertTo-SecureString $PasswordForUsers -AsPlainText -Force
 
 
 # Creates an OrganisationalUnit named Users
-New-ADOrganisationalUnit -Name _USERS
+New-ADOrganisationalUnit -Name _USERS 
 
 
 # Splitting of the names into sub-strings
 foreach ($n in $UserNamesList) {
     $first = $n.Split(" ")[0].ToLower()
-    $last = $n.Split(" ")[1].ToLower()
-           
+    $last = $n.Split(" ")[1].ToLower()        
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+       
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
 
     }
